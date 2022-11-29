84c8a4808eeb7a65382d802487dec5df671e8dc6
Aydin <70951773+AydinT222@users.noreply.github.com>
2022-11-29 13:11:52 -0500

    a

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,8 +1,22 @@
+import java.awt.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 public class VoidReturn {
+   public static String fullName = "John Doe";
+   public static void main(String args[]) {
+      int total = addNums();
 
-   public static void main(String args[]){
-       
+      System.out.println("Your total is: " + total);
 
    }
 
+   public static int addNums() {
+      System.out.println("We are going to add two numbers");
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Please enter them below");
+      int numA = scan.nextInt();
+      int numB = scan.nextInt();
+      return numA + numB;
+   }
 }
